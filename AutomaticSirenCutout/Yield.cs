9abe3626fb62cbc457d86e0f2277db401e885397
Rich Dunne <richdunne@yahoo.com>
2020-09-19 15:10:22 -0600

    Added local variables to make reading easier.  Added Dismiss function.  Added more null ref checks.

--- AutomaticSirenCutout/Yield.cs
+++ AutomaticSirenCutout/Yield.cs
@@ -12,28 +12,28 @@ namespace AutomaticSirenCutout
         internal static void YieldMain()
         {
             Vector3 garage = new Vector3(396.42f, -970.0003f, -99.36382f);
+            Vector3 rearPos = new Vector3();
+            float collectionRadius = 7;
             AppDomain.CurrentDomain.DomainUnload += TerminationHandler;
 
             while (true)
             {
-                if (Game.LocalPlayer.Character.Position.DistanceTo(garage) > 5f && Game.LocalPlayer.Character.LastVehicle && Game.LocalPlayer.Character.LastVehicle.IsSirenOn && Game.LocalPlayer.Character.LastVehicle.Speed == 0f)// && !bleftBlocker)
+                if (Game.LocalPlayer.Character.Position.DistanceTo(garage) > 5f && Game.LocalPlayer.Character.LastVehicle && Game.LocalPlayer.Character.LastVehicle.IsSirenOn && Game.LocalPlayer.Character.LastVehicle.Speed == 0f)
                 {
-                    foreach(Vehicle v in GetNearbyVehicles(Game.LocalPlayer.Character.LastVehicle.RearPosition, 10f).Where(v => v && v != Game.LocalPlayer.Character.LastVehicle && v.IsEngineOn && v.IsOnAllWheels && (Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) < 90f || Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) > 200f) && !yieldingVehicles.Contains(v)))
+                    rearPos = Game.LocalPlayer.Character.LastVehicle.GetOffsetPosition(new Vector3(0, -4f, 0));
+                    //Rage.Native.NativeFunction.Natives.DRAW_MARKER(1, rearPos, 0, 0, 0, 0, 0, 0, collectionRadius, collectionRadius, 1f, 255, 255, 255, 100, false, false, 0, false, 0, 0, false);
+                    foreach(Vehicle v in Game.LocalPlayer.Character.GetNearbyVehicles(16))
+                    //foreach (Vehicle v in GetNearbyVehicles(Game.LocalPlayer.Character.LastVehicle.GetOffsetPosition(new Vector3(0, -4f, 0)), 5f))
                     {
-                        v.IsPersistent = true;
-                        if (!v.HasDriver)
+                        if (v && v.FrontPosition.DistanceTo(rearPos) <= collectionRadius && v != Game.LocalPlayer.Character.LastVehicle && v.IsEngineOn && v.IsOnAllWheels && !v.IsSirenOn && !v.IsTrailer && !v.IsTrain && (Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) < 90f || Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) > 200f) && !yieldingVehicles.Contains(v))
                         {
-                            v.CreateRandomDriver();
-                            while (!v.HasDriver)
-                            {
-                                GameFiber.Yield();
-                            }
-                        }
-                        v.Driver.IsPersistent = true;
+                            SetVehicleAndDriverPersistence(v);
+                            yieldingVehicles.Add(v);
+                            Game.LogTrivialDebug($"{v.Model.Name} added to collection.");
 
-                        yieldingVehicles.Add(v);
-                        Game.LogTrivialDebug($"{v.Model.Name} added to collection.");
-                        v.PerformYieldTasks();
+                            GameFiber YieldTasksFiber = new GameFiber(() => v.PerformYieldTasks());
+                            YieldTasksFiber.Start();
+                        }
                     }
                 }
                 GameFiber.Yield();
@@ -41,45 +41,76 @@ namespace AutomaticSirenCutout
 
             Vehicle[] GetNearbyVehicles(Vector3 OriginPosition, float radius)
             {
-                return (from x in World.GetAllVehicles() where !x.IsTrailer && x.DistanceTo(OriginPosition) < radius select x).ToArray();
+                return (from v in World.GetAllVehicles() where v && v.FrontPosition.DistanceTo(OriginPosition) <= radius && v != Game.LocalPlayer.Character.LastVehicle && v.IsEngineOn && v.IsOnAllWheels && !v.IsSirenOn && !v.IsTrailer && !v.IsTrain && (Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) < 90f || Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) > 200f) select v).ToArray();
             }
+        }
+
+        private static void SetVehicleAndDriverPersistence(Vehicle v)
+        {
+            v.IsPersistent = true;
+            if (!v.HasDriver)
+            {
+                v.CreateRandomDriver();
+                while (v && !v.HasDriver)
+                {
+                    GameFiber.Yield();
+                }
+                if (!v)
+                {
+                    Game.LogTrivialDebug($"Vehicle is null");
+                    return;
+                }
+            }
+            v.Driver.IsPersistent = true;
         }
 
         private static void PerformYieldTasks(this Vehicle v)
         {
-            if (!v || !v.Driver)
+            if (!v)
             {
-                Game.LogTrivialDebug($"Vehicle or driver is null");
+                Game.LogTrivialDebug($"Vehicle is null");
+                return;
+            }
+            if (!v.Driver)
+            {
+                Game.LogTrivialDebug($"Driver is null");
                 return;
             }
 
             if (v.Speed < 1f)
             {
                 v.Driver.Tasks.PerformDrivingManeuver(v, VehicleManeuver.GoForwardWithCustomSteeringAngle, 1).WaitForCompletion();
-            }
-            v.Driver.Tasks.CruiseWithVehicle(5f, (VehicleDrivingFlags)558);
-            //Game.LogTrivialDebug($"{v.Model.Name} should be cruising.");
-
-            GameFiber.StartNew(() =>
-            {
-                while (v && v.Driver && Game.LocalPlayer.Character.LastVehicle && v.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle.RearPosition) < 10f)
+                if (v && v.Driver)
                 {
-                    if (v && v.Driver && v.Speed < 1f)
+                    v.Driver.Tasks.CruiseWithVehicle(5f, (VehicleDrivingFlags)558);
+                }
+            }
+
+            while (v && v.Driver && Game.LocalPlayer.Character.LastVehicle && v.FrontPosition.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle.GetOffsetPosition(new Vector3(0, -4f, 0))) < 7f)
+            {
+                if (v && v.Driver && v.Speed < 1f)
+                {
+                    v.Driver.Tasks.PerformDrivingManeuver(v, VehicleManeuver.GoForwardWithCustomSteeringAngle, 1).WaitForCompletion();
+                    if(v && v.Driver)
                     {
-                        v.Driver.Tasks.PerformDrivingManeuver(v, VehicleManeuver.GoForwardWithCustomSteeringAngle, 1).WaitForCompletion();
                         v.Driver.Tasks.CruiseWithVehicle(5f, (VehicleDrivingFlags)558);
                     }
-                    //Game.LogTrivialDebug($"Waiting for {v.Model.Name} to leave the area.");
-                    GameFiber.Sleep(100);
                 }
+                Game.LogTrivialDebug($"Waiting for {v.Model.Name} to leave the area.");
+                GameFiber.Sleep(100);
+            }
 
-                yieldingVehicles.Remove(v);
-                v.Driver.Tasks.Clear();
-                Game.LogTrivialDebug($"{v.Model.Name} removed from collection.");
+            Dismiss(v);
+        }
 
-                v.Driver.Dismiss();
-                v.Dismiss();
-            });
+        private static void Dismiss(this Vehicle v)
+        {
+            yieldingVehicles.Remove(v);
+            v.Driver.Tasks.Clear();
+            Game.LogTrivialDebug($"{v.Model.Name} removed from collection.");
+
+            v.Driver.Dismiss();
+            v.Dismiss();
         }
 
         internal static void TerminationHandler(object sender, EventArgs e)
