4f15fe368a65d95384ce408372396673032a91de
Rich Dunne <richdunne@yahoo.com>
2020-09-22 13:24:59 -0600

    Added check to determine if police vehicle to be yielded is ambient or not.  Added custom steering angle to encourage vehicles to yield left

--- AutomaticSirenCutout/Yield.cs
+++ AutomaticSirenCutout/Yield.cs
@@ -24,6 +24,15 @@ namespace AutomaticSirenCutout
                     //Rage.Native.NativeFunction.Natives.DRAW_MARKER(1, rearPos, 0, 0, 0, 0, 0, 0, collectionRadius, collectionRadius, 1f, 255, 255, 255, 100, false, false, 0, false, 0, 0, false);
                     foreach(Vehicle v in Game.LocalPlayer.Character.GetNearbyVehicles(16).Where(v => v && v.FrontPosition.DistanceTo(rearPos) <= collectionRadius && v != Game.LocalPlayer.Character.LastVehicle && v.IsEngineOn && v.IsOnAllWheels && !v.IsSirenOn && !v.IsTrailer && !v.IsTrain && (Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) < 90f || Math.Abs(Game.LocalPlayer.Character.LastVehicle.Heading - v.Heading) > 200f) && !yieldingVehicles.Contains(v)))
                     {
+                        if (v.HasSiren)
+                        {
+                            GameFiber.Sleep(7000);
+                            if (v && !v.HasDriver && v.FrontPosition.DistanceTo(rearPos) <= collectionRadius)
+                            {
+                                Game.LogTrivialDebug($"{v.Model.Name} is an emergency vehicle and hasn't moved in 7 seconds.  It might be a backup unit, disregard.");
+                                continue;
+                            }
+                        }
                         SetVehicleAndDriverPersistence(v);
                         yieldingVehicles.Add(v);
                         Game.LogTrivialDebug($"{v.Model.Name} added to collection.");
@@ -68,21 +77,14 @@ namespace AutomaticSirenCutout
                 return;
             }
 
-            if (v.Speed < 1f)
-            {
-                v.Driver.Tasks.PerformDrivingManeuver(v, VehicleManeuver.GoForwardWithCustomSteeringAngle, 1).WaitForCompletion();
-                if (v && v.Driver)
-                {
-                    v.Driver.Tasks.CruiseWithVehicle(5f, (VehicleDrivingFlags)558);
-                }
-            }
+            SetCustomSteeringAngle(v);
 
             while (v && v.Driver && Game.LocalPlayer.Character.LastVehicle && v.FrontPosition.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle.GetOffsetPosition(new Vector3(0, -4f, 0))) < 7f)
             {
                 if (v && v.Driver && v.Speed < 1f)
                 {
                     v.Driver.Tasks.PerformDrivingManeuver(v, VehicleManeuver.GoForwardWithCustomSteeringAngle, 1).WaitForCompletion();
-                    if(v && v.Driver)
+                    if (v && v.Driver)
                     {
                         v.Driver.Tasks.CruiseWithVehicle(5f, (VehicleDrivingFlags)558);
                     }
@@ -90,14 +92,40 @@ namespace AutomaticSirenCutout
                 Game.LogTrivialDebug($"Waiting for {v.Model.Name} to leave the area.");
                 GameFiber.Sleep(100);
             }
+            if (v)
+            {
+                Dismiss(v);
+            }
+        }
 
-            Dismiss(v);
+        private static void SetCustomSteeringAngle(Vehicle v)
+        {
+            if (v.Speed < 1f)
+            {
+                if (v.Heading < 45f)
+                {
+                    v.SteeringAngle = 360f + (v.Heading - 45f);
+                }
+                else
+                {
+                    v.SteeringAngle = v.Heading - 45f;
+                }
+                v.Driver.Tasks.PerformDrivingManeuver(v, VehicleManeuver.GoForwardWithCustomSteeringAngle, 2).WaitForCompletion();
+
+                if (v && v.Driver)
+                {
+                    v.Driver.Tasks.CruiseWithVehicle(5f, (VehicleDrivingFlags)558);
+                }
+            }
         }
 
         private static void Dismiss(this Vehicle v)
         {
             yieldingVehicles.Remove(v);
-            v.Driver.Tasks.Clear();
+            if (v.Driver)
+            {
+                v.Driver.Tasks.Clear();
+            }
             Game.LogTrivialDebug($"{v.Model.Name} removed from collection.");
 
             v.Driver.Dismiss();
