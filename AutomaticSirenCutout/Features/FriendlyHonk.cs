6f54ee86fa1a6a92f73714b7912c962963f07445
Rich Dunne <richdunne@yahoo.com>
2020-11-29 12:29:00 -0700

    Reorganized features into Features folder.

---
+++ AutomaticSirenCutout/Features/FriendlyHonk.cs
@@ -0,0 +1,39 @@
+﻿using Rage;
+using System.Linq;
+
+namespace AutomaticSirenCutout.Features
+{
+    public class FriendlyHonk
+    {
+        public static void Honk()
+        {
+            while (true)
+            {
+                if (Game.LocalPlayer.Character.CurrentVehicle && Game.LocalPlayer.Character.CurrentVehicle.HasSiren)
+                {
+                    if (Game.IsKeyDown(Settings.KeyboardYelp) || Game.IsControllerButtonDown(Settings.ControllerYelp))
+                    {
+                        foreach (Vehicle v in World.GetAllVehicles().Where(v => v && v != Game.LocalPlayer.Character.CurrentVehicle && v.HasSiren && v.HasDriver && v.DistanceTo2D(Game.LocalPlayer.Character.Position) <= 30f && !v.IsSirenOn))
+                        {
+                            Game.LogTrivialDebug("[Friendly Honk] Player is yelping siren at another cop.");
+                            GameFiber.Sleep(500);
+                            v.BlipSiren(false);
+                            break;
+                        }
+                    }
+                    else if (Game.IsKeyDown(Settings.KeyboardHorn) || Game.IsControllerButtonDown(Settings.ControllerHorn))
+                    {
+                        foreach (Vehicle v in World.GetAllVehicles().Where(v => v && v != Game.LocalPlayer.Character.CurrentVehicle && v.HasSiren && v.HasDriver && v.DistanceTo2D(Game.LocalPlayer.Character.Position) <= 30f && !v.IsSirenOn))
+                        {
+                            Game.LogTrivialDebug("[Friendly Honk] Player is honking at another cop.");
+                            GameFiber.Sleep(500);
+                            Rage.Native.NativeFunction.Natives.START_VEHICLE_HORN(v, 10, "NORMAL", false);
+                            break;
+                        }
+                    }
+                }
+                GameFiber.Yield();
+            }
+        }
+    }
+}
---
+++ AutomaticSirenCutout/Features/TrafficLightControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Rage;
+
+namespace AutomaticSirenCutout.Features
+{
+    internal class TrafficLightControl
+    {
+        internal static void TLC()
+        {
+            List<uint> trafficSignalHashes = new List<uint> { 3639322914, 862871082, 865627822, 1043035044 };
+
+            while (true)
+            {
+                GameFiber.Sleep(500);
+                if(Game.LocalPlayer.Character.CurrentVehicle && Game.LocalPlayer.Character.CurrentVehicle.IsSirenOn)
+                {
+                    foreach (Entity entity in World.GetEntities(Game.LocalPlayer.Character.Position, 50f, GetEntitiesFlags.ConsiderAllObjects).Where(o => o && trafficSignalHashes.Contains(o.Model.Hash)))
+                    {
+                        if (entity.Heading <= Game.LocalPlayer.Character.LastVehicle.Heading + 45f && entity.Heading >= Game.LocalPlayer.Character.LastVehicle.Heading - 45f)
+                        {
+                            // Turn light green
+                            Rage.Native.NativeFunction.Natives.SET_ENTITY_TRAFFICLIGHT_OVERRIDE(entity, 0);
+                        }
+                        else
+                        {
+                            // Turn light red
+                            Rage.Native.NativeFunction.Natives.SET_ENTITY_TRAFFICLIGHT_OVERRIDE(entity, 1);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
